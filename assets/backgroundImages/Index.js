2c7ef0e1a8ea0f3d48a5ffd747317525aa1f9782
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-15 17:59:12 -0300

    Añade carpeta con algunas imagenes del clima, para que cambie en base a la temperatura

---
+++ assets/backgroundImages/Index.js
@@ -0,0 +1,6 @@
+const haze = require('./Img/haze.jpg')
+const rainy = require('./Img/rainy.jpg')
+const snow = require('./Img/snow.jpg')
+const sunny = require('./Img/sunny.jpg')
+
+export { haze, rainy, snow, sunny}
