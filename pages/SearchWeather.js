f112d99894bb1d79f22538953f25c38c58174722
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-17 21:17:03 -0300

    Añade conexíon a la base de datos con axios, agrega a la lista de ciudades guardadas

--- pages/SearchWeather.js
+++ pages/SearchWeather.js
@@ -7,17 +7,29 @@ import {
   Alert,
   ActivityIndicator,
   Dimensions,
+  TouchableOpacity,
 } from "react-native";
 import InputSearch from "../components/InputSearch/InputSearch";
 import ListData from "../components/ListData/ListData";
 import Maps from "../components/Maps/Maps";
 import { haze, rainy, snow, sunny } from "../assets/backgroundImages/Index";
 import Weather from "../components/Weather/Weather";
+import { AntDesign } from "@expo/vector-icons";
+import axios from "axios";
 
 const SearchWeather = ({ navigator }) => {
   const [backgroundImage, setBackgroundImage] = useState(null);
-
+  const [showIconAction, setShowIconAction] = useState(true);
   const [resultado, setResultado] = useState(null);
+  const [sendData, setSendData] = useState({
+    name: "",
+    country: "",
+    lat: "",
+    lon: "",
+    temp_max: "",
+    temp_min: "",
+    temp: "",
+  });
   const [loaded, setLoaded] = useState(true);
 
   const fetchClima = async (ciudad) => {
@@ -44,18 +56,65 @@ const SearchWeather = ({ navigator }) => {
       const { weather } = resultado;
       const [{ main }] = weather;
       console.log("main ", main);
+      setSendData({
+        nameCity: resultado.name,
+        country: resultado.sys.country,
+        lat: resultado.coord.lat,
+        lon: resultado.coord.lon,
+        temp_max: resultado.main.temp_max,
+        temp_min: resultado.main.temp_min,
+        temp: resultado.main.temp,
+      });
+      
+      formData.append("nameCity", resultado.name);
+      formData.append("country", resultado.sys.country);
+      formData.append("lat", resultado.coord.lat);
+      formData.append("lon", resultado.coord.lon);
+      formData.append("temp_max", resultado.main.temp_max);
+      formData.append("temp_min", resultado.main.temp_min);
+      formData.append("temp", resultado.main.temp);
       setBackgroundImage(getBackgroundImage(main));
     }
   }, [resultado]);
   useEffect(() => {
-    fetchClima("mendoza");
+    fetchClima("Buenos Aires");
   }, []);
   const showAlert = () => {
     Alert.alert("Error", "No hay resultados, intenta con otra ciudad", [
       { text: "OK" },
     ]);
   };
+  const config = {
+    headaers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+  };
+  const handleChangeIcon = async () => {
+    console.log("nameCity", resultado.name);
+    console.log("country", resultado.sys.country);
+    console.log("lat", resultado.coord.lat);
+    console.log("lon", resultado.coord.lon);
+    console.log("temp_max", resultado.main.temp_max);
+    console.log("temp_min", resultado.main.temp_min);
+    console.log("temp", resultado.main.temp);
+    console.log("resultado click name ", resultado.name);
 
+    console.log("SendDATA===>", sendData);
+    try {
+      const { data } = await axios.post(
+        "https://bd-app-clima.vercel.app/listweather",
+        sendData);
+      console.log("recibido");
+      console.log("respo => ", data);
+      // HACER EL MENSAJE DE GUARDADO
+      // al guardarse, que se pueda eliminar
+      setSendData({})
+    } catch (error) {
+      console.log(error);
+    }
+    setShowIconAction(!showIconAction);
+  };
   const getBackgroundImage = (weather) => {
     if (weather === "Snow") return snow;
     if (weather === "Clear") return sunny;
@@ -79,6 +138,16 @@ const SearchWeather = ({ navigator }) => {
             resizeMode="cover"
           >
             <View style={styles.containerSearch}>
+              <TouchableOpacity
+                onPress={() => handleChangeIcon()}
+                style={styles.iconAction}
+              >
+                <AntDesign
+                  name={showIconAction ? "pluscircleo" : "checkcircleo"}
+                  size={24}
+                  color={showIconAction ? "#000" : "green"}
+                />
+              </TouchableOpacity>
               <Weather
                 fetchClima={fetchClima}
                 resultado={resultado}
@@ -99,7 +168,6 @@ const styles = StyleSheet.create({
   },
   backgroundImg: {
     flex: 1,
-    backgroundColor: "red",
     width: Dimensions.get("screen").width,
   },
   containerLoaded: {
@@ -127,4 +195,11 @@ const styles = StyleSheet.create({
   containerBtn: {
     marginVertical: 10,
   },
+  iconAction: {
+    display: "flex",
+    position: "absolute",
+    right: 20,
+    marginTop: 60,
+  },
 });
+const formData = new FormData();
