a852a6e1066a1f72dd498aac16d346dea7f9b082
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-21 06:26:07 -0300

    Añade vista del mapa

--- components/Maps/Maps.js
+++ components/Maps/Maps.js
@@ -3,16 +3,28 @@ import { Dimensions, StyleSheet, View } from 'react-native';
 import MapView from "react-native-maps"
 const Maps = () =>{
     return(
-        <View> 
-            <MapView style={styles.map}/>
+        <View style={styles.container}> 
+            <MapView
+             style={styles.map}
+             initialRegion={{
+                latitude: 39.675775,
+                longitude: -75.768287,
+                latitudeDelta: 0.0122,
+                longitudeDelta: 0.0121,
+              }}
+            />
         </View>
     )
 }
 
 export default Maps;
 const styles = StyleSheet.create({
+    container:{
+        backgroundColor: 'transparent'        
+    },
     map:{
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        width:Dimensions.get('window').width-40,
+        height: Dimensions.get('window').height/5,
+      
     }
 })
