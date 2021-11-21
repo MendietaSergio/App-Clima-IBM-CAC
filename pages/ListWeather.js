90afd39aaf560fffa9c1132a3815006e25ac03bb
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-20 22:30:32 -0300

    añade api axios delete, envio id para eliminar ciudad eliminada

--- pages/ListWeather.js
+++ pages/ListWeather.js
@@ -1,85 +1,111 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import ListData from "../components/ListData/ListData";
-const ListWeather = ({ navigator }) => {
-  const [dataItem, setDataItem] = useState([
-    {
-      id: "1",
-      localidad: "Avellaneda",
-      ciudad: "Buenos Aires",
-      fecha: "12:05 PM",
-      temMax: 23,
-      temMin: 15,
-    },
-    {
-      id: "2",
-      localidad: "Avellaneda",
-      ciudad: "Buenos Aires",
-      fecha: "12:05 PM",
-      temMax: 23,
-      temMin: 15,
-    },
-    {
-      id: "3",
-      localidad: "Avellaneda",
-      ciudad: "Buenos Aires",
-      fecha: "12:05 PM",
-      temMax: 23,
-      temMin: 15,
-    },
-    {
-      id: "4",
-      localidad: "Avellaneda",
-      ciudad: "Buenos Aires",
-      fecha: "12:05 PM",
-      temMax: 23,
-      temMin: 15,
-    },
-    {
-      id: "5",
-      localidad: "Quilmes",
-      ciudad: "Buenos Aires",
-      fecha: "12:05 PM",
-      temMax: 23,
-      temMin: 15,
-    },
-  ]);
-  const [showMaps, setShowMaps] = useState(false);
+import axios from "axios";
 
+const ListWeather = ({ navigation }) => {
+  const [showMaps, setShowMaps] = useState(false);
+  const [resultado, setResultado] = useState(null);
+  const [loaded, setLoaded] = useState(true);
+  const getListWeather = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://bd-app-clima.vercel.app/listweather"
+      );
+      setResultado(data);
+      // setLoaded(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const config = {
+    headaers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+  };
+  const deleteCity = async (id) =>{
+    console.log("eliminado id => ", id);
+    try {
+      await axios.delete(
+        `https://bd-app-clima.vercel.app/delete/${id}`
+      )
+      .then(resp =>{
+        console.log("data => ", resp.data.message);
+        getListWeather()
+      })
+    } catch (error) {
+      console.log(error);
+    } 
+  }
+  useEffect(() => {
+    if (resultado != null) {
+      // console.log("resultado=> ", resultado);
+      setLoaded(false);
+    }
+  }, [resultado]);
+  useEffect(() => {
+    getListWeather();
+  }, []);
   return (
     <>
-      {dataItem.lenght > 0 ? (
-        <Text>Usted no tiene guardado</Text>
+      {loaded ? (
+        <View style={styles.containerLoaded}>
+          <ActivityIndicator color="gray" size={36} />
+        </View>
       ) : (
         <>
-          {showMaps ? (
+          {resultado != null ? (
             <>
-              <View style={styles.containerList}>
-                <Maps />
-              </View>
-            </>
-          ) : (
-            <>
-              <View style={styles.containerList}>
-                <Text style={styles.textTitle}>
-                  Lista de ubicaciones guardadas
-                </Text>
+              {showMaps ? (
+                <>
+                  <View style={styles.containerList}>
+                    <Maps />
+                  </View>
+                </>
+              ) : (
+                <>
+                  <View style={styles.containerList}>
+                    <Text style={styles.textTitle}>
+                      Lista de ubicaciones guardadas
+                    </Text>
 
-                <FlatList
-                  style={styles.list}
-                  data={dataItem}
-                  renderItem={({ item }) => <ListData item={item} />}
-                  keyExtractor={(dataItem) => dataItem.id}
+                    <FlatList
+                      style={styles.list}
+                      data={resultado}
+                      renderItem={({ item }) => (
+                        <ListData item={item} deleteCity={deleteCity} />
+                      )}
+                      keyExtractor={(item) => item._id}
+                    />
+                  </View>
+                </>
+              )}
+              <View style={styles.containerBtn}>
+                <Button
+                  title={showMaps ? "Ver lista" : "Ver mapa"}
+                  onPress={() => setShowMaps(!showMaps)}
                 />
               </View>
             </>
+          ) : (
+            <View style={styles.containerInfo}>
+              <Text>No hay ninguno guardado</Text>
+              <View style={styles.btnRedireccion}>
+                <Button
+                  title={"Buscar"}
+                  onPress={() => navigation.navigate("SearchWeather")}
+                />
+              </View>
+            </View>
           )}
-          <View style={styles.containerBtn}>
-            <Button
-              title={showMaps ? "Ver lista" : "Ver mapa"}
-              onPress={() => setShowMaps(!showMaps)}
-            />
-          </View>
         </>
       )}
     </>
@@ -93,6 +119,12 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 2.5,
   },
+  containerInfo: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   textTitle: {
     fontSize: 14,
     fontWeight: "bold",
@@ -105,5 +137,13 @@ const styles = StyleSheet.create({
   },
   containerBtn: {
     marginVertical: 10,
+  },
+  btnRedireccion: {
+    marginVertical: 40,
+  },
+  containerLoaded: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
