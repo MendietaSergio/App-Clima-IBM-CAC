12781e903547ee76b9a08b13d8c1b2a24dfd2f00
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-28 03:29:28 -0300

    AÑade solucion a la visualizacion de imagen en base al pronostico, limpieza de console.log

--- pages/DetailWeather.js
+++ pages/DetailWeather.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  ImageBackground,
+  Text,
+  Dimensions,
+} from "react-native";
 import Weather from "../components/Weather/Weather";
+import {
+  cloudy,
+  rainy,
+  snow,
+  sunny,
+  haze,
+} from "../assets/backgroundImages/Index";
+import { color } from "react-native-reanimated";
 
 const DetailWeather = ({ route }) => {
   const item = route.params;
   const [resultado, setResultado] = useState(null);
   const [loaded, setLoaded] = useState(true);
+  const [backgroundImage, setBackgroundImage] = useState(null);
 
   const fetchClima = async (ciudad) => {
     const apiID = "8eaae482f433816013eb58a3b326c92d";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiID}`;
-    console.log(url);
     try {
       const resp = await fetch(url);
       if (resp.status == 200) {
         const data = await resp.json();
-        console.log("data =>", data);
         setResultado(data);
-        setLoaded(false);
       } else {
         setResultado(null);
         showAlert();
@@ -27,10 +40,25 @@ const DetailWeather = ({ route }) => {
     }
   };
   useEffect(() => {
+    if (resultado !== null) {
+      const [{ main }] = resultado.weather;
+      setBackgroundImage(getBackgroundImage(main));
+      setLoaded(false);
+    }
+  }, [resultado]);
+  const getBackgroundImage = (weather) => {
+    if (weather === "Snow") return snow;
+    if (weather === "Clear") return sunny;
+    if (weather === "Rain") return rainy;
+    if (weather === "Haze") return haze;
+    if (weather === "Clouds") return cloudy;
+  };
+  useEffect(() => {
     if (item) {
       fetchClima(item);
     }
   }, [item]);
+  let colorText = (backgroundImage !== rainy ?(backgroundImage !== sunny ? "white" : "black"): "black");
   return (
     <View style={styles.container}>
       {loaded ? (
@@ -38,9 +66,21 @@ const DetailWeather = ({ route }) => {
           <ActivityIndicator color="gray" size={36} />
         </View>
       ) : (
-        <View>
-          <Weather viewInputSearch={false} resultado={resultado} />
-        </View>
+        <>
+          <ImageBackground
+            style={styles.backgroundImg}
+            source={backgroundImage}
+            resizeMode="cover"
+          >
+            <View>
+              <Weather
+                viewInputSearch={false}
+                resultado={resultado}
+                colorText={colorText}
+              />
+            </View>
+          </ImageBackground>
+        </>
       )}
     </View>
   );
@@ -49,12 +89,20 @@ const DetailWeather = ({ route }) => {
 export default DetailWeather;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-      },
-    containerLoaded: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-})
+  container: {
+    flex: 1,
+  },
+  data: {
+    flex: 1,
+    marginVertical: 10,
+  },
+  containerLoaded: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  backgroundImg: {
+    flex: 1,
+    width: Dimensions.get("screen").width,
+  },
+});
