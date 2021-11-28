c59700b8bfdaad1252d58f44d9c0befa0fb0ff32
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-28 03:31:21 -0300

    Añade cambio en variable de img

--- assets/backgroundImages/Index.js
+++ assets/backgroundImages/Index.js
@@ -2,5 +2,6 @@ const cloudy = require('./Img/cloudy.jpeg')
 const rainy = require('./Img/rainy.jpg')
 const snow = require('./Img/snow.jpg')
 const sunny = require('./Img/sunny.jpg')
+const haze = require('./Img/haze.jpg')
 
-export { cloudy, rainy, snow, sunny}
+export { cloudy, rainy, snow, sunny, haze}
